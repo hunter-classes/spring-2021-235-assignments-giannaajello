b0d46181d8b9a5aff4b5cce140c78e6287d0bbb0
giannaajello <64743235+giannaajello@users.noreply.github.com>
2021-02-05 10:29:09 -0500

    Update hanoi.cpp

--- recursion/hanoi.cpp
+++ recursion/hanoi.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 //base case: n=1 -- 1 disc !!
 
-void hanoi(int n, std::string first, std::string second, std::string third) //number of disks is the input
+void hanoi(int n, char src, char dst, char tmp) //number of disks is the input
 {
     if (n == 1)
     {
-        std::cout << "Move disk 1 from  " << first << " to " << second << std::endl;
+        std::cout << "Move disk 1 from  " << src << " to " << dst << std::endl;
         return; //returns nothing because it is a void function !! it is just printing
     }
-    hanoi(n - 1, first, third, second); //n-1 is in directions, you lose a disk for every move bc its moving from the rod
-    std::cout << "Move disk " << n << " from " << first << " to " << second << std::endl;
-    hanoi(n - 1, third, second, first);
+    hanoi(n - 1, src, tmp, dst); //n-1 is in directions, you lose a disk for every move bc its moving from the rod
+    std::cout << "Move disk " << n << " from " << src << " to " << dst << std::endl;
+    hanoi(n - 1, tmp, dst, src);
 }
 
 int main()
 {
-    int n = 3;
-    hanoi(n, "Rod A", "Rod B", "Rod C");
+    int n = 2;
+    hanoi(n, 'A', 'B', 'C');
     return 0;
 }
+
